d2b514f03e9ace6fa03451bfd4369e3f3d43468e
jibonilla03 <jimmy.bonilla@bairesdev.com>
2021-06-27 16:41:47 -0600

    Add 80 to security group, and user data to set up basic http server

--- webserver/__main__.py
+++ webserver/__main__.py
@@ -11,15 +11,23 @@ ami = aws.get_ami(most_recent="true",
 
 # Add security group to allow SSH access
 group = aws.ec2.SecurityGroup('webserver-secgrp',
-    description='Enable SSH access',
+    description='Enable HTTP access',
     ingress=[
-        { 'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0'] }
+        { 'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0'] },
+        { 'protocol': 'tcp', 'from_port': 80, 'to_port': 80, 'cidr_blocks': ['0.0.0.0/0'] }
     ])
+
+user_data = """
+#!/bin/bash
+echo "Hello, World!" > index.html
+nohup python -m SimpleHTTPServer 80 &
+"""
 
 # Provision EC2 instance
 server = aws.ec2.Instance('webserver-www',
     instance_type=size,
     vpc_security_group_ids=[group.id], # reference security group from above
+    user_data=user_data,
     ami=ami.id)
 
 pulumi.export('publicIp', server.public_ip)
