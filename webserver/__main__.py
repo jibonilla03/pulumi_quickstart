322927726310a17d66cd169bf659384278b3a053
jibonilla03 <jimmy.bonilla@bairesdev.com>
2021-06-27 16:27:55 -0600

    This version works and provisioned EC2 on default VPC

--- webserver/__main__.py
+++ webserver/__main__.py
@@ -5,7 +5,7 @@ import pulumi_aws as aws
 
 # Define variable to and get AMI
 size = 't2.micro'
-ami = aws.ec2.getAmi(most_recent="true",
+ami = aws.get_ami(most_recent="true",
                   owners=["137112412989"],
                   filters=[{"name":"name","values":["amzn-ami-hvm-*"]}])
 
@@ -13,15 +13,14 @@ ami = aws.ec2.getAmi(most_recent="true",
 group = aws.ec2.SecurityGroup('webserver-secgrp',
     description='Enable SSH access',
     ingress=[
-        {'protocol': 'tcp', 'from_port' : 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0']}
+        { 'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0'] }
     ])
 
 # Provision EC2 instance
 server = aws.ec2.Instance('webserver-www',
-    isinstance_type=size,
+    instance_type=size,
     vpc_security_group_ids=[group.id], # reference security group from above
     ami=ami.id)
 
 pulumi.export('publicIp', server.public_ip)
 pulumi.export('publicHostName', server.public_dns)
-    
