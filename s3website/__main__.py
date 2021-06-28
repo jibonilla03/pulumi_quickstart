18a06ce349828dfdb16d3363ca4b626f81f159b0
jibonilla03 <jimmy.bonilla@bairesdev.com>
2021-06-27 19:49:42 -0600

    Serve a static website out of content in an S3 bucket

--- s3website/__main__.py
+++ s3website/__main__.py
@@ -1,10 +1,48 @@
-"""An AWS Python Pulumi program"""
+"""Host a Static Website on Amazon S3"""
+import json
+import mimetypes
+import os
 
-import pulumi
+from pulumi import export, FileAsset
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+web_bucket = s3.Bucket('s3-website-bucket',
+    website=s3.BucketWebsiteArgs(
+        index_document="index.html",
+    ))
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+content_dir = "www" # directory for content files
+
+# For each file in the directory, create an S3 object stored in `siteBucket`
+for file in os.listdir(content_dir):
+    filepath = os.path.join(content_dir,file)
+    mime_type, _= mimetypes.guess_type(filepath)
+    obj = s3.BucketObject(file,
+        bucket=web_bucket.id,
+        source=FileAsset(filepath),
+        content_type=mime_type)
+
+def public_read_policy_for_bucket(bucket_name):
+    return json.dumps({
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                f"arn:aws:s3:::{bucket_name}/*",
+            ]
+        }]
+    })
+
+bucket_name = web_bucket.id
+bucket_policy = s3.BucketPolicy("bucket-policy",
+    bucket=bucket_name,
+    policy=bucket_name.apply(public_read_policy_for_bucket))
+
+# Create a stack export for bucket name
+export('bucket_name', web_bucket.id)
+export('website_url', web_bucket.website_endpoint)
